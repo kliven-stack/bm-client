1e9b07e6a5d1ac2bf2c364ccb3ca37b5abe582f4
Nevilk <nevilk@Nevilks-MacBook-Pro.local>
2026-06-08 11:54:08 +0800

    first commit

---
+++ public/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://swjbzspqcnldckavaaky.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3amJ6c3BxY25sZGNrYXZhYWt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1Mzg4MTYsImV4cCI6MjA5NjExNDgxNn0.wA4pfzG_CdjQw7MEvDKBkxPwliSI12NwWaWS5z-4aVk';
+
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
